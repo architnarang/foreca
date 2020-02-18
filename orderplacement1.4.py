499699266ca46b932c27341288d5e8df2c50aa5b
aaryanshsahay <53135035+aaryanshsahay@users.noreply.github.com>
2020-02-18 21:41:03 +0530

    Update orderplacement1.4.py

--- orderplacement1.4.py
+++ orderplacement1.4.py
@@ -1,3 +1,7 @@
+#sample test code sub-part of the entire code
+#it checks which element doesnt belong in the pre-defined list.
+#obviously only works for integers,gotta change to str for string values.
+
 lst1=[1,2,3]
 a = input().split(' ')
 
