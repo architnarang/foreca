79aec6e3e86c11c2b92fb6898a624699b3e3ced8
aaryanshsahay <53135035+aaryanshsahay@users.noreply.github.com>
2020-02-18 21:47:09 +0530

    Update orderseperator1.3.py

--- orderseperator1.3.py
+++ orderseperator1.3.py
@@ -1,3 +1,7 @@
+#checks the input and prints spelling incorrect if input doesnt match with the elements of the pre defied (lst1) list.
+#this code only takes in non numeric data.
+#the output instead of showing 'banana':spelling incorrect shows 'b':spelling incorrect 'a':spelling incorrect...so on 
+#id like your help in tackling that issue 
 lst1=['tomato','potato','onion']
 a1=input().strip()
 a=a1.split(',')
